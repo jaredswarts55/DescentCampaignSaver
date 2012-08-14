34fe02eb5a53ea667ce13a694e7cbd337817ecf4
Jared Swarts <jared.swarts@gmail.com>
2012-08-14 10:02:29 -0500

    Added license

---
+++ DescentCampaignSaver/Descent/Overlord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using DescentCampaignSaver.Descent.Relics;
+
+namespace DescentCampaignSaver.Descent
+{
+    public class Overlord
+    {
+        public ObservableCollection<OverlordRelic> OverlordRelics { get; set; }
+    }
+}
---
+++ DescentCampaignSaver/DescentCampaign.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DescentCampaignSaver.Descent;
+
+namespace DescentCampaignSaver
+{
+    public class DescentCampaign
+    {
+        public List<Player> Players { get; set; }
+    }
+}
